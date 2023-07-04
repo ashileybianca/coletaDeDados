44bd71868bacabde5457f0d2548673d9467c40cd
Ashiley Bianca <ashbycontato@gmail.com>
2023-07-04 14:12:40 -0300

    Refatorar código para melhorar coleta de links de vídeos em LIBRAS
    Nesta alteração, realizei uma refatoração no código para aprimorar a coleta de links de vídeos em LIBRAS. As melhorias incluem a remoção de tempos fixos de espera, a formatação adequada dos textos das opções, a verificação de erros nas páginas e a adição dos links de vídeos coletados à lista correspondente.
    
    No entanto, é importante ressaltar que alguns links inválidos não foram coletados corretamente nesta versão. Será necessário encontrar uma alternativa adequada para lidar com esses casos e garantir a coleta completa dos links de todos os vídeos em LIBRAS disponíveis.

--- acessibilidadebrasil.py
+++ acessibilidadebrasil.py
@@ -1,4 +1,3 @@
-
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -43,12 +42,125 @@ for letra in letras_menu:
         lista_option_texts.append(option_text)
 
 for texto in lista_option_texts:
-    links_videos.append("http://www.acessibilidadebrasil.org.br/libras_3/public/media/palavras/videos/" + texto + "Sm_Prog001.mp4")
+    url = "http://www.acessibilidadebrasil.org.br/libras_3/public/media/palavras/videos/" + texto + "Sm_Prog001.mp4"
+    time.sleep(0.4)
+    # Verifica se a página contém a mensagem de erro
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    error_message = soup.find(text="Desculpe, mas aconteceu um erro.")
+
+    if error_message:
+        print(f"A página {url} apresenta a mensagem de erro.")
+    else:
+        links_videos.append(url)
 
 print(links_videos)
 
 driver.quit()
 
+'''import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import unidecode
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome()
+
+driver.get('http://www.acessibilidadebrasil.org.br/libras_3/')
+
+letras_menu = driver.find_elements(By.XPATH, '//div[@id="filter-letter"]/ul/li/a')
+
+lista_option_texts = []
+links_videos = []
+
+for letra in letras_menu:
+    letra.click()
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    options = soup.find_all('option')[1:]
+
+    for option in options:
+        option_style = option.get('style')
+        if option and not option_style or 'display: none' not in option_style:  # Verifica se o objeto option existe e está visível
+            option_text = option.text
+            option_text = option_text.lower()
+            option_text = unidecode.unidecode(option_text)
+            option_text = option_text.replace(" ", "_")
+            lista_option_texts.append(option_text)
+
+for texto in lista_option_texts:
+    url = "http://www.acessibilidadebrasil.org.br/libras_3/public/media/palavras/videos/" + texto + "Sm_Prog001.mp4"
+    time.sleep(0.4)
+    # Verifica se a página contém a mensagem de erro
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    error_message = soup.find(text="Desculpe, mas aconteceu um erro.")
+
+    if error_message:
+        print(f"A página {url} apresenta a mensagem de erro.")
+    else:
+        links_videos.append(url)
+
+print(links_videos)
+
+driver.quit()
+
+'''
+'''import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import unidecode
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome()
+
+
+Melhorias que ainda serão realizadas:
+- transformar em funcao
+- alguns links nao foram coletados corretamente com essa alternativa, entao vamos voltar para
+a alternativa 1 de cliclar atraves do selenium e coletar links.
+
+
+
+driver.get('http://www.acessibilidadebrasil.org.br/libras_3/')
+
+time.sleep(5)
+
+letras_menu = driver.find_elements(By.XPATH, '//div[@id="filter-letter"]/ul/li/a')
+
+lista_option_texts = []
+links_videos = []
+
+for letra in letras_menu:
+    letra.click()  
+    
+    time.sleep(1)  
+
+    html = driver.page_source 
+    soup = BeautifulSoup(html, 'html.parser')
+
+    options = soup.find_all('option')[1:]
+    
+    for option in options:
+        option_text = option.text
+        option_text = option_text.lower()  
+        option_text = unidecode.unidecode(option_text)
+        option_text = option_text.replace(" ", "_") 
+        lista_option_texts.append(option_text)
+
+for texto in lista_option_texts:
+    links_videos.append("http://www.acessibilidadebrasil.org.br/libras_3/public/media/palavras/videos/" + texto + "Sm_Prog001.mp4")
+
+print(links_videos)
+
+driver.quit()
+'''
 '''import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
