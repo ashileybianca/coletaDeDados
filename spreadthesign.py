0400f53143c7172358207354ff15bc1144c4775f
Ashiley Bianca <ashbycontato@gmail.com>
2023-07-05 17:17:59 -0300

    Foram feitas melhorias no código para obter vídeos do site Spread the Sign. As principais alterações incluem:
    - Adição da biblioteca equests para realizar requisições HTTP.
    - Utilização da função obter_links_categorias para obter os links das categorias.
    - Implementação da função obter_qtd_paginas_categoria para obter a quantidade de páginas em cada categoria.
    - Criação da função gerar_vetor_urls_cada_categoria para gerar os URLs de cada categoria com base na quantidade de páginas.
    - Melhorias no processo de obtenção dos vídeos, incluindo a correção de normalização de caracteres e remoção de caracteres de controle.
    - Ajustes na lógica de iteração das categorias e páginas para obter todos os vídeos do site.
    - Atualização da estrutura do DataFrame resultante.
    - Adição de opções de exibição do DataFrame.
    - Salvar os dados em um arquivo CSV.

--- spreadthesign.py
+++ spreadthesign.py
@@ -1,137 +1,156 @@
 import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 import unicodedata
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from urllib.parse import urljoin
 import csv
 
-'''
-DÚVIDAS:
+categorias = 'https://www.spreadthesign.com/pt.br/search/by-category/'
+urls_e_qtd_paginas = []
 
-*** Como fazer ele percorrer todos os topicos e SUBTÓPICOS?
-- Fazer manualmente?
-- Passando como:
-    urls_paginas = [('https://www.spreadthesign.com/pt.br/search/by-category/', 100)]
- alguns links são invalidos.
+def obter_links_categorias(url):
+    response = requests.get(url)
+    html = response.text
 
- *** Como normalizar os caracteres corretamente? 
-- Pedi ajuda para o chat gpt que me indicou a biblioteca unicodedata
-- Mass alguns acentos são perdidos
- 
-urls_paginas = [
-    ('https://www.spreadthesign.com/pt.br/search/by-category/398/lingua-de-sinais-para-iniciantes/?q=&p=', 6),
-    ('https://www.spreadthesign.com/pt.br/search/by-category/1/diversos/?q=&p=', 23),
-    ('https://www.spreadthesign.com/pt.br/search/by-category/37/frases/?q=&p=', 2),
-    ('https://www.spreadthesign.com/pt.br/search/by-category/86/religiao/?q=&p=', 9),
-    ('https://www.spreadthesign.com/pt.br/search/by-category/13/pedagogia/?q=&p=', 15),
-    ('https://www.spreadthesign.com/pt.br/search/by-category/13/pedagogia/?q=&p=', 15),
-    ('https://www.spreadthesign.com/pt.br/search/by-category/28/lingua/?q=&p=', 62),
-    ('https://www.spreadthesign.com/pt.br/search/by-category/113/arte-e-entretenimento/?q=&p=', 28),
-    ('https://www.spreadthesign.com/pt.br/search/by-category/46/estudos-sociais/?q=&p=', 55)
-    (),
-    # Link, numero de pags
-]
+    links = []
 
-urls_paginas = [('https://www.spreadthesign.com/pt.br/search/by-category/', 100)]
-'''
+    soup = BeautifulSoup(html, 'html.parser')
+    searchable_elements = soup.find_all(class_='js-searchable')
 
-urls_paginas = [
-    ('https://www.spreadthesign.com/pt.br/search/by-category/398/lingua-de-sinais-para-iniciantes/?q=&p=', 1)]
+    for element in searchable_elements:
+        anchor_tags = element.find_all('a')
+        for tag in anchor_tags:
+            if 'href' in tag.attrs:
+                links.append('https://www.spreadthesign.com'+tag['href'])
 
+    return links
 
-def gerar_vetor_urls(urls_paginas):
-    urls = []
+def obter_qtd_paginas_categoria(url):
+    qtd_paginas = 1
 
-    for url, num_pages in urls_paginas:
-        for page in range(1, num_pages + 1):
-            url_pagina = url + str(page)
-            urls.append(url_pagina)
+    while True:
+        response = requests.get(url)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
 
-    return urls
+        link_prox_pagina = soup.find('div', class_='col-xs-4 search-pager-next').find('a')
 
-#ooooi
+        if link_prox_pagina: #se existir link pra prox
+            qtd_paginas += 1
+            url_prox_pagina = link_prox_pagina['href']
+            url = urljoin(url, url_prox_pagina)
+            # combina a URL base atual (url) com o caminho relativo da próxima página (url_prox_pagina)
+        else:
+            break # quando não existir mais
 
-def obter_videos_site(urls):
-    options = Options()
-    options.headless = True #Ativado para que navegador não abra.
+    return qtd_paginas
 
-    driver = webdriver.Chrome(options=options)
+def gerar_vetor_urls_cada_categoria(urls_e_qtd_paginas):
 
-    lista_videos = []
+    urls_categoria = []
 
-    for url in urls:
+    for url, num_paginas in urls_e_qtd_paginas:
+        for pagina in range(1, num_paginas + 1):
+            url_pagina = url + str(pagina)
+            urls_categoria.append(url_pagina)
 
-        driver.get(url)
+    return urls_categoria
 
-        time.sleep(5)  # delay para carregar a página
+def gerar_link_e_qtd_paginas(url):
 
-        html = driver.page_source  # pega o html
-        soup = BeautifulSoup(html, 'html.parser')  # organiza
+    links = obter_links_categorias(url)
 
-        # Encontrar todas as divs com a classe search-result-title (onde estão as palavras)
-        divs = soup.find_all('div', class_='search-result-title')
+    for link in links:
+        qtd_paginas = obter_qtd_paginas_categoria(link)
+        print(link,qtd_paginas)
+        urls_e_qtd_paginas.append((link, qtd_paginas))
 
-        for div in divs:
-            atags = div.find_all('a')  # atags = todos os "a"
+    return urls_e_qtd_paginas
 
-            for a in atags:
-                # Excluir a tag <small> do conteúdo de cada "a", pois não queremos "substantivo", "verbo", etc
-                small_tag = a.find('small')
-                if small_tag:
-                    small_tag.decompose()
+def obter_videos_site(categorias):
+    urls_e_qtd_paginas = gerar_link_e_qtd_paginas(categorias)
 
-                # Extrair o texto do elemento 'a'
-                text = a.get_text(strip=True) 
-                # strip=True remove espaços em branco extras no início e no final do texto.
+    for url, num_paginas in urls_e_qtd_paginas:
+        urls_cada_categoria = gerar_vetor_urls_cada_categoria([(url, num_paginas)])
 
-                # Remover caracteres de controle e normalizar os caracteres Unidecode (Feito pelo ChatGPT)
-                text = "".join(ch for ch in text if unicodedata.category(ch)[0] != "C")
-                text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8')
+        options = Options()
+        #options.headless = True Ativado para que o navegador não seja aberto.
 
-                # Obter o link do elemento 'a'
-                href = a['href']
+        driver = webdriver.Chrome(options=options)
 
-                # Abrir o link em uma nova aba
-                driver.execute_script("window.open(arguments[0], '_blank');", href)
-                time.sleep(1)  # Aguardar um segundo após o clique
+        lista_videos = []
 
-                # Alternar para a nova aba
-                driver.switch_to.window(driver.window_handles[-1])
+        for url in urls_cada_categoria:
+            driver.get(url)
+            time.sleep(5)  # delay para carregar a página
 
-                # Obter o HTML da nova página
-                video_html = driver.page_source
-                video_soup = BeautifulSoup(video_html, 'html.parser')
+            html = driver.page_source  # pega o html
+            soup = BeautifulSoup(html, 'html.parser')  # organiza
 
-                # Encontrar a div que contém o vídeo
-                video_div = video_soup.find('div', class_='col-md-7')
+            # Encontrar todas as divs com a classe search-result-title (onde estão as palavras)
+            divs = soup.find_all('div', class_='search-result-title')
 
-                if video_div:
-                    # Verificar se há um elemento de vídeo presente
-                    video_tag = video_div.find('video')
-                    if video_tag:
-                        # Obter o valor do atributo 'src' do vídeo
-                        video_src = video_tag.get('src')
-                        lista_videos.append((text, video_src))
+            for div in divs:
+                atags = div.find_all('a')  # atags = todos os "a"
+
+                for a in atags:
+                    # Excluir a tag <small> do conteúdo de cada "a", pois não queremos "Substantivo", "verbo", etc.
+                    small_tag = a.find('small')
+                    if small_tag:
+                        small_tag.decompose()
+
+                    # Extrair o texto do elemento 'a'
+                    text = a.get_text(strip=True) 
+                    # strip=True remove espaços em branco extras no início e no final do texto.
+
+                    # Remover caracteres de controle e normalizar os caracteres Unidecode
+                    text = "".join(ch for ch in text if unicodedata.category(ch)[0] != "C")
+                    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8')
+
+                    # Obter o link do elemento 'a'
+                    href = a['href']
+
+                    # Abrir o link em uma nova aba
+                    driver.execute_script("window.open(arguments[0], '_blank');", href)
+                    time.sleep(1)  # Aguardar um segundo após o clique
+
+                    # Alternar para a nova aba
+                    driver.switch_to.window(driver.window_handles[-1])
+
+                    # Obter o HTML da nova página
+                    video_html = driver.page_source
+                    video_soup = BeautifulSoup(video_html, 'html.parser')
+
+                    # Encontrar a div que contém o vídeo
+                    video_div = video_soup.find('div', class_='col-md-7')
+
+                    if video_div:
+                        # Verificar se há um elemento de vídeo presente
+                        video_tag = video_div.find('video')
+                        if video_tag:
+                            # Obter o valor do atributo 'src' do vídeo
+                            video_src = video_tag.get('src')
+                            lista_videos.append((text, video_src))
+                        else:
+                            lista_videos.append((text, 'null'))
                     else:
                         lista_videos.append((text, 'null'))
-                else:
-                    lista_videos.append((text, 'null'))
 
-                # Fechar a nova aba e voltar para a aba anterior
-                driver.close()
-                driver.switch_to.window(driver.window_handles[0])
+                    # Fechar a nova aba e voltar para a aba anterior
+                    driver.close()
+                    driver.switch_to.window(driver.window_handles[0])
 
-    driver.quit()
-    df_videos = pd.DataFrame(lista_videos, columns=['Palavra', 'Link'])
-    df_videos['Instituicao'] = 'Spread the Sign' 
+        driver.quit()
+        print(lista_videos)
+        df_videos = pd.DataFrame(lista_videos, columns=['Palavra', 'Link'])
+        df_videos['Instituicao'] = 'Spread the Sign' 
 
     return df_videos
 
-urls = gerar_vetor_urls(urls_paginas)
-print(urls)
-
-df_videos = obter_videos_site(urls)
+df_videos = obter_videos_site(categorias)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', None)
@@ -151,3 +170,4 @@ with open(nome_arquivo, 'w', newline='', encoding='utf-8') as arquivo_csv:
         escritor.writerow({campo: formato[campo](valor) for campo, valor in linha.items()})
 
 print("Dados salvos em arquivo CSV:", nome_arquivo)
+
